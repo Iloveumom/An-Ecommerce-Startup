8e52e201cd1ccd320a88ef9d5ba84305bb6b7727
Iloveumom <jtndrbhatia93@gmail.com>
2026-01-09 21:37:14 +0530

    contoller task

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,6 @@ const router=express.Router();
 router.get("/",productContoller.getProducts);
 router.get("/:productID",productContoller.getProductById);
 router.post("/",productContoller.addProducts);
-router.put("/:productID",productContoller.editProductById);
-router.delete("/:productID",productContoller.deleteProductById);
+// router.put("/:productID",productContoller.editProductById);
+// router.delete("/:productID",productContoller.deleteProductById);
 module.exports=router;
